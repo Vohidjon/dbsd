3622881784b44125c098db6b0b3eb9cd1dbc41dc
sherxon <sheraliobidov1199@gmail.com>
2017-06-10 10:59:49 -0700

    leftover

--- 00003951_DBSD_CW2/Controllers/HomeController.cs
+++ 00003951_DBSD_CW2/Controllers/HomeController.cs
@@ -16,53 +16,5 @@ namespace _00003951_DBSD_CW2.Controllers
             IList<FlowerCategory> list = categoryManager.GetFlowerCategories();
             return View(list);
         }
-
-        //[HttpGet]
-        //public ActionResult Apply(int id)
-        //{
-        //    VacancyManager manager = new VacancyManager();
-        //    Vacancy vacancy = manager.GetVacancyById(id);
-        //    ViewBag.VacancyTitle = vacancy.VacancyTitle;
-        //    ViewBag.VacancyId = vacancy.VacancyId;
-        //    return View();
-        //}
-
-        //[HttpPost]
-        //public ActionResult Apply(int id, Application application)
-        //{
-        //    try
-        //    {
-        //        ApplicationManager appManager = new ApplicationManager();
-        //        StageManager stageManager = new StageManager();
-        //        application.VacancyId = id;
-
-        //        application.StageId = stageManager.GetFirst().StageId;
-        //        application.ApplicationCreatedAt = DateTime.Now;
-        //        appManager.CreateApplication(application);
-
-        //        return RedirectToAction("Success");
-        //    } catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
-        //public ActionResult Success()
-        //{
-        //    return View();
-        //}
-        //public ActionResult About()
-        //{
-        //    ViewBag.Message = "Your application description page.";
-
-        //    return View();
-        //}
-
-        //public ActionResult Contact()
-        //{
-        //    ViewBag.Message = "Your contact page.";
-
-        //    return View();
-        //}
     }
 }
